8ff2207a063baf0801f1992f50d0f99ea7a9e9ce
etj <etj@geo-solutions.it>
2022-03-22 15:21:52 +0100

    Fix assets declarations

--- ckanext/umbria/plugin.py
+++ ckanext/umbria/plugin.py
@@ -12,7 +12,7 @@ class UmbriaPlugin(plugins.SingletonPlugin):
     def update_config(self, config_):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
-        toolkit.add_resource('assets', 'umbria')
+        toolkit.add_resource('public', 'umbria')  # path, webasset name
 
     # ITemplateHelpers
     def get_helpers(self):
