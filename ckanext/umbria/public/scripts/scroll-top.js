882142533a5a7c1f83aefc07590a1eb6aa81b540
DavidQuartz <quartzdavid@gmail.com>
2022-04-05 11:59:41 +0000

    Fixed header on scroll

--- ckanext/umbria/public/scripts/scroll-top.js
+++ ckanext/umbria/public/scripts/scroll-top.js
@@ -2,19 +2,26 @@
  * Scroll to top button
  */
 
-$(document).load(function () {
+window.addEventListener('load', function () {
     $(document).scroll(handleScroll)
     
     function handleScroll() {
         var scrollToTopBtn = $('.move-top');
         var rootElement = $(':root')[0];
         var scrollTotal = rootElement.scrollHeight - rootElement.clientHeight;
+        var header = $('header.masthead');
+        var headerHeight = header.height();
         if ((rootElement.scrollTop / scrollTotal) > 0.30) {
                 // Show button
             scrollToTopBtn.addClass("showBtn");
                 } else {
                 // Hide button
             scrollToTopBtn.removeClass("showBtn");
-            }
+        }
+        if ((rootElement.scrollTop / headerHeight) > 1) {
+            $('div.collapse.navbar-collapse').addClass('fixed-top');
+        } else {
+            $('div.collapse.navbar-collapse').removeClass('fixed-top');
+        }
     }
 });
