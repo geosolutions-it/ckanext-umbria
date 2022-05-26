5b0a2ae9a66faf1e6f4f04d512c941189ff526dd
DavidQuartz <quartzdavid@gmail.com>
2022-05-26 11:05:26 +0000

    fixed header bug

--- ckanext/umbria/public/scripts/scroll-top.js
+++ ckanext/umbria/public/scripts/scroll-top.js
@@ -3,25 +3,35 @@
  */
 
 window.addEventListener('load', function () {
-    $(document).scroll(handleScroll)
+    var stickyNav = $('#sticky-top');
+    var stickNavWrapper = $('#sticky-nav-wrapper');
+    var currStickyPos = stickyNav.offset().top + window.pageYOffset;
+    var navHeight = stickyNav.height();
+    $(document).scroll(handleScroll);
     
     function handleScroll() {
         var scrollToTopBtn = $('.move-top');
         var rootElement = $(':root')[0];
         var scrollTotal = rootElement.scrollHeight - rootElement.clientHeight;
-        var header = $('header.masthead');
-        var headerHeight = header.height();
+
         if ((rootElement.scrollTop / scrollTotal) > 0.30) {
                 // Show button
-            scrollToTopBtn.addClass("showBtn");
+            scrollToTopBtn.addClass('showBtn');
                 } else {
                 // Hide button
-            scrollToTopBtn.removeClass("showBtn");
+            scrollToTopBtn.removeClass('showBtn');
         }
-        if ((rootElement.scrollTop / headerHeight) > 1) {
-            $('div.collapse.navbar-collapse').addClass('fixed-top');
-        } else {
-            $('div.collapse.navbar-collapse').removeClass('fixed-top');
+
+        // fix header to the top
+         if(window.pageYOffset > currStickyPos) {
+             stickyNav.addClass('fixed-top');
+             stickNavWrapper.css({marginBottom: `${navHeight}px`})
+         } else {
+             if ((window.pageYOffset + navHeight) > currStickyPos) {
+                 stickNavWrapper.css({ marginBottom: 0 });
+              }
+             stickyNav.removeClass('fixed-top');
+             stickNavWrapper.css({ marginBottom: 0 });
         }
     }
 });
